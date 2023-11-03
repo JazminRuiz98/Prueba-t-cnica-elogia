1a8185353072962d0527159066ff3454149fc62c
JazminRuiz98 <aida.jazmin.ruiz.orbe@gmail.com>
2023-11-03 02:14:16 -0600

    actualización del código, el programa puede extraer la ruta absoluta y la relativa, además que extraer la extensión del archivo

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 // md links y aquí se llaman las microfunciones
-const app = require('./lib/app.js')
+
+const { validatePath, extensionDoc, readFile, findLinks } = require('./lib/app');
+const path = require('path');
+
+function mdLinks(filePath) {
+    return new Promise((resolve, reject) => {
+        const absolutePath = validatePath(filePath);
+        if (!absolutePath) {
+            reject(new Error('Lo sentimos, la ruta no es válida'));
+        }
+        if (!extensionDoc(absolutePath)) {
+            reject(new Error('lo sentimos, el documento no tiene las extensiones validas'));
+        }
+
+        readFile(absolutePath)
+            .then((data) => {
+                const links = findLinks(data, absolutePath);
+                resolve(links);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+module.exports = mdLinks;
+
+/*const app = require('./lib/app.js')
 const result = mdLinks(ruta);
 new Promise((resolve, reject) => {
 
-})
+})*/
---
+++ test.js
@@ -0,0 +1,7 @@
+const { validatePath } = require('./lib/app');
+
+const relativePath = './some/example.md'; // Aquí debes usar una ruta relativa válida
+const absolutePath = validatePath(relativePath);
+
+console.log('Ruta relativa:', relativePath);
+console.log('Ruta absoluta:', absolutePath);
